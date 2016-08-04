54fa029519c093fa4524ffd544040223ae31720d
rudyyazdi <rudyyazdi@gmail.com>
2016-08-04 17:52:11 +1000

    gulp done

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const FILE_NAME = "ruuuud"
+
+var gulp       = require('gulp'),
+    clean      = require('gulp-clean'),
+    concat     = require('gulp-concat'),
+    watch      = require('gulp-watch'),
+    uglify     = require('gulp-uglify'),
+    exec       = require('child_process').exec;
+
+gulp.task('clean', function () {
+  return gulp.src('build/*.*', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('cancatUglify', function () {
+	return gulp.src([
+    'source/one.js',
+    'source/two.js',
+    'source/three.js'])
+	.pipe(concat(`${FILE_NAME}.min.js`))
+	.pipe(uglify())
+	.pipe(gulp.dest('build'));
+});
+
+gulp.task('run', function () {
+  exec(`node build/${FILE_NAME}.min.js`, function (err, stdout, stderr) {
+    console.log(stdout);
+  });
+})
+
+gulp.task('default', ['clean', 'cancatUglify']);
