d7dbc1f76e79b5cb5eaee686815129fcd3e0e6ba
rudyyazdi <rudyyazdi@gmail.com>
2016-08-06 13:45:11 +1000

    cleanup gulp js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 let gulp       = require('gulp');
 let clean      = require('gulp-clean');
 let concat     = require('gulp-concat');
-let watch      = require('gulp-watch');
 let uglify     = require('gulp-uglify');
 let shell      = require('shelljs');
 
-const FILE_NAME = shell.exec('whoami')
+const FILE_NAME = shell.exec('whoami');
 
 gulp.task('clean', function () {
   return gulp.src('build/*.*', { read: false })
@@ -18,12 +17,12 @@ gulp.task('cancatUglify', ['clean'], function () {
       'source/two.js',
       'source/three.js'])
   	.pipe(concat(`${FILE_NAME}.min.js`))
-  	.pipe(uglify({mangle: {toplevel: true}}))
+  	.pipe(uglify({ mangle: { toplevel: true } }))
   	.pipe(gulp.dest('build'));
 });
 
 gulp.task('run', ['clean', 'cancatUglify'], function () {
   shell.exec(`node 'build/${FILE_NAME}.min.js'`);
-})
+});
 
 gulp.task('default', ['clean', 'cancatUglify' ,'run']);
