3983131ff8d3e88a4b64c83584ee3cd0ba32544b
rudyyazdi <rudyyazdi@gmail.com>
2016-08-08 20:21:02 +1000

    cleanup js

--- source/three.js
+++ source/three.js
@@ -1,3 +1,3 @@
 // This is the third file !!!
-const My_long_constant_name = 'build_tool';
-console.log(My_long_constant_name);
+const MyLongConstantName = 'build_tool';
+console.log(MyLongConstantName);
